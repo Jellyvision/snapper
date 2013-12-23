af5f75930beee79ceb8686383e936b46c6d169cf
Jeremy Kahn <jeremyckahn@gmail.com>
2013-12-23 14:19:33 -0600

    Saves images with incrementing integer file names.

--- php/upload.php
+++ php/upload.php
@@ -16,11 +16,33 @@ function console_log ($obj) {
 // CONSTANTS
 //
 
-$DESTINATION_DIR = '../images';
+$BASE_DIR = '../';
+$DESTINATION_DIR = 'images';
 
-console_log($_FILES['userfile']);
+//console_log($_FILES['userfile']);
 
-$new_file_name = $DESTINATION_DIR . '/' . $_FILES['userfile']['name'];
+$files = scandir($BASE_DIR . $DESTINATION_DIR);
+
+$largest_int_found = 0;
+foreach($files as $file) {
+  $file_chunks = explode('.', $file);
+  $int_name = (int)($file_chunks[0]);
+
+  // If the int doesn't convert cleanly back into the original value, the file
+  // name wasn't an int and should be skipped.
+  if ((string)$int_name !== $file_chunks[0]) {
+    continue;
+  }
+
+  if ($int_name > $largest_int_found) {
+    $largest_int_found = $int_name;
+  }
+}
+
+$path = $BASE_DIR . $DESTINATION_DIR . '/';
+// TODO: Determine $file_suffix by examining the MIME type.
+$file_suffix = end(explode('.', $_FILES['userfile']['name']));
+$new_file_name = $path . (string)($largest_int_found + 1) . '.' . $file_suffix;
 
 if (move_uploaded_file($_FILES['userfile']['tmp_name'], $new_file_name)) {
   echo 'File uploaded as ' . $new_file_name;
